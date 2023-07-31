080c211e3ac7cfe0daed82a02683b1f65b5ba209
Lauren Swersky <lauren.swersky@gmail.com>
2023-07-31 19:14:41 -0400

    filter rendering

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import React, { useState } from 'react';
-import Map from './components/Map.jsx';
-import Dropdown from './components/Dropdown.jsx';
-import './styles/App.css';
-import Side from './components/Side.jsx';
-
+import React, { useState, useEffect } from "react";
+import Map from "./components/Map.jsx";
+import Papa from "papaparse";
+import Dropdown from "./components/Dropdown.jsx";
+import "./styles/App.css";
+import Side from "./components/Side.jsx";
 
 function App() {
   const [data, setData] = useState([]);
@@ -11,17 +11,75 @@ function App() {
   const [markers, setShowMarkers] = useState(true);
   const [initialData, setInitialData] = useState([]);
   const [mapIsLoaded, setMapIsLoaded] = useState(false);
+  const [markersArr, setMarkersArr] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  // Parse CSV data
+  useEffect(() => {
+    fetch("/Etsy2023G.csv")
+      .then((response) => response.blob())
+      .then((blob) => {
+        Papa.parse(blob, {
+          header: true,
+          delimiter: ",",
+          skipEmptyLines: "greedy",
+          complete: function (results) {
+            const addressData = results.data.map((item) => ({
+              item: item["Item Name"],
+              buyer: item.buyer,
+              date: item["Date Paid"],
+              name: item["Ship Name"].split(" ")[0],
+              quantity: item.Quantity,
+              address1: item["Ship Address1"],
+              city: item["Ship City"],
+              state: item["Ship State"],
+              latitude: parseFloat(item.Latitude),
+              longitude: parseFloat(item.Longitude),
+            }));
+            setData(addressData);
+            setInitialData(addressData);
+            setLoading(false);
+          },
+        });
+        console.log("DATA!!!!", initialData);
+      });
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>
+  }
 
 
   //TODO: refactor so i don't pass all this down
   return (
     <div className="App">
       <header className="App-header">
-        <Side className="Side" setShowMarkers={setShowMarkers} setShowHeatMap={setShowHeatMap} showHeatMap={showHeatMap} data={data} setData={setData} initialData={initialData} setMapIsLoaded={setMapIsLoaded} mapIsLoaded={mapIsLoaded}/>
+        <Side
+          className="Side"
+          setShowMarkers={setShowMarkers}
+          setShowHeatMap={setShowHeatMap}
+          showHeatMap={showHeatMap}
+          data={data}
+          setData={setData}
+          initialData={initialData}
+          setMapIsLoaded={setMapIsLoaded}
+          mapIsLoaded={mapIsLoaded} setMarkersArr={setMarkersArr} markersArr={markersArr}
+        />
         <h2>Etsy Sales Map</h2>
         <Dropdown className="Dropdown" />
       </header>
-      <Map data={data} setData={setData} showHeatMap={showHeatMap} setShowHeatMap={setShowHeatMap} markers={markers} setShowMarkers={setShowMarkers} initialData={initialData} setInitialData={setInitialData} setMapIsLoaded={setMapIsLoaded} mapIsLoaded={mapIsLoaded}/>
+      <Map
+        data={data}
+        setData={setData}
+        showHeatMap={showHeatMap}
+        setShowHeatMap={setShowHeatMap}
+        markers={markers}
+        setShowMarkers={setShowMarkers}
+        initialData={initialData}
+        setInitialData={setInitialData}
+        setMapIsLoaded={setMapIsLoaded}
+        mapIsLoaded={mapIsLoaded} setMarkersArr={setMarkersArr} markersArr={markersArr}
+      />
     </div>
   );
 }
@@ -30,12 +88,13 @@ export default App;
 
 
 
+
+
 // import React, { useState } from 'react';
 // import Map from './components/Map.jsx';
 // import Dropdown from './components/Dropdown.jsx';
 // import './styles/App.css';
 // import Side from './components/Side.jsx';
-
 
 // function App() {
 //   const [data, setData] = useState([]);
@@ -58,4 +117,3 @@ export default App;
 // }
 
 // export default App;
-
--- src/components/Side.jsx
+++ src/components/Side.jsx
@@ -1,20 +1,38 @@
-import { Checkbox, FormControlLabel, Typography } from '@mui/material';
-import Tooltip from '@mui/material/Tooltip';
-import Box from '@mui/material/Box';
-import Divider from '@mui/material/Divider';
-import IconButton from '@mui/material/IconButton';
-import Avatar from '@mui/material/Avatar';
-import { useState, useEffect, Fragment } from 'react';
-import { Drawer, Icon, Button, ListItem, ListItemText, ListItemAvatar } from '@mui/material';
-import FilterListIcon from '@mui/icons-material/FilterList';
-import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
-import { styled, useTheme } from '@mui/material/styles';
+import { Checkbox, FormControlLabel, Typography } from "@mui/material";
+import Tooltip from "@mui/material/Tooltip";
+import Box from "@mui/material/Box";
+import Divider from "@mui/material/Divider";
+import IconButton from "@mui/material/IconButton";
+import Avatar from "@mui/material/Avatar";
+import { useState, useEffect, Fragment } from "react";
+import {
+  Drawer,
+  Icon,
+  Button,
+  ListItem,
+  ListItemText,
+  ListItemAvatar,
+} from "@mui/material";
+import FilterListIcon from "@mui/icons-material/FilterList";
+import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
+import { styled, useTheme } from "@mui/material/styles";
 
 // TODO: Make sure drawer doesn't cover infowindow / recenter map
 // TODO: Avoid rerender on every click
 
-const Side = ({ setShowMarkers, setShowHeatMap, showHeatMap, data, setData, handleMarkers, initialData, setMapIsLoaded, mapIsLoaded }) => {
-
+const Side = ({
+  setShowMarkers,
+  setShowHeatMap,
+  showHeatMap,
+  data,
+  setData,
+  handleMarkers,
+  initialData,
+  setMapIsLoaded,
+  mapIsLoaded,
+  markersArr,
+  setMarkersArr
+}) => {
   const [open, setOpen] = useState(false);
   const [selectAllChecked, setSelectAllChecked] = useState(true);
   const [checkedItems, setCheckedItems] = useState({});
@@ -23,40 +41,61 @@ const Side = ({ setShowMarkers, setShowHeatMap, showHeatMap, data, setData, hand
   useEffect(() => {
     const filterData = () => {
       if (Object.keys(checkedItems).length > 0) {
-        const filteredData = initialData.filter(item =>
-          checkedItems[item.item]);
+        const filteredData = initialData.filter(
+          (item) => checkedItems[item.item]
+        );
         setData(filteredData);
       } else {
         setData(initialData);
       }
     };
-    console.log('data.length:', data)
     filterData();
   }, [checkedItems]);
 
 
   const handleSelection = (e, photoTitle) => {
     // Toggle the checked status of the photo item
-    e.preventDefault()
-    setCheckedItems((prevCheckedItems) => ({
-      ...prevCheckedItems,
-      [photoTitle]: !prevCheckedItems[photoTitle],
-    }));
+          e.preventDefault();
+
+          markersArr.forEach((marker) => {
+            marker.setMap(null);
+          });
+
+    // Check if already selected, reset to empty obj
+    if (checkedItems[photoTitle]) {
+      let checkedItemCopy = { ...checkedItems };
+      delete checkedItemCopy[photoTitle];
+      setCheckedItems(checkedItemCopy);
+    } else {
+
+
+      setCheckedItems((prevCheckedItems) => ({
+        ...prevCheckedItems,
+        [photoTitle]: !prevCheckedItems[photoTitle],
+      }));
+    }
     // Check if any checkbox is unchecked to set "Show All" to false
-    setSelectAllChecked(Object.values(checkedItems).every((isChecked) => isChecked));
+    setSelectAllChecked(
+      Object.values(checkedItems).every((isChecked) => isChecked)
+    );
 
     // If mobile, close the side bar upon selection
     if (isMobile) {
       setOpen(false);
     }
+
   };
 
 
+  // Drawer close
   const handleDrawerClose = () => {
     setOpen(false);
   };
 
   const handleShowAll = () => {
+    markersArr.forEach((marker) => {
+      marker.setMap(null);
+    });
     setData(initialData);
     setShowHeatMap(true);
     setShowMarkers(true);
@@ -68,61 +107,67 @@ const Side = ({ setShowMarkers, setShowHeatMap, showHeatMap, data, setData, hand
     }
   };
 
-  const DrawerHeader = styled('div')(({ theme }) => ({
-    display: 'flex',
+  const DrawerHeader = styled("div")(({ theme }) => ({
+    display: "flex",
     padding: theme.spacing(0, 1),
     ...theme.mixins.toolbar,
-    justifyContent: 'flex-end',
+    justifyContent: "flex-end",
   }));
 
   const photos = [
     {
       src: "/caminoColorful.avif",
-      title: 'Camino de Santiago Bandages, Plasters, 30 pcs',
+      title: "Camino de Santiago Bandages, Plasters, 30 pcs",
     },
     {
-      src: 'https://i.etsystatic.com/12475356/r/il/72703d/4344046976/il_75x75.4344046976_az6t.jpg',
-      title: 'Bandages (2-pack), Camino de Santiago Plasters, 30 pcs per box'
+      src: "https://i.etsystatic.com/12475356/r/il/72703d/4344046976/il_75x75.4344046976_az6t.jpg",
+      title: "Bandages (2-pack), Camino de Santiago Plasters, 30 pcs per box",
     },
     {
-      src: 'https://i.etsystatic.com/12475356/c/2000/2000/0/0/il/63d30f/4416907678/il_75x75.4416907678_3bma.jpg',
-      title: 'Bandages (3-pack), Camino de Santiago Plasters, 30 pcs per box'
+      src: "https://i.etsystatic.com/12475356/c/2000/2000/0/0/il/63d30f/4416907678/il_75x75.4416907678_3bma.jpg",
+      title: "Bandages (3-pack), Camino de Santiago Plasters, 30 pcs per box",
     },
     {
-      src: '/bandagesx2.avif',
-      title: 'Bandages, Plasters (2 pack) - Camino de Santiago'
-    }, {
-      src: '	/italyListingPic.avif',
-      title: 'Italian Bandages, Italy Plasters, 30 pcs'
+      src: "/bandagesx2.avif",
+      title: "Bandages, Plasters (2 pack) - Camino de Santiago",
     },
     {
-      src: '/italyListingPicx2.avif',
-      title: 'Italian Bandages (2-pack), Italy Plasters, 30 pcs each'
+      src: "	/italyListingPic.avif",
+      title: "Italian Bandages, Italy Plasters, 30 pcs",
     },
     {
-      src: 'italyListingPicx3.jpg',
-      title: 'Italian Bandages (3-pack), Italy Plasters, 30 pcs each'
+      src: "/italyListingPicx2.avif",
+      title: "Italian Bandages (2-pack), Italy Plasters, 30 pcs each",
+    },
+    {
+      src: "italyListingPicx3.jpg",
+      title: "Italian Bandages (3-pack), Italy Plasters, 30 pcs each",
     },
   ];
 
   return (
     <>
-      <Tooltip title="Filter by product" placement="right" arrow style={{ marginLeft: 4 }}>
+      <Tooltip
+        title="Filter by product"
+        placement="right"
+        arrow
+        style={{ marginLeft: 4 }}
+      >
         <Button
           id="filter-button"
           sx={{
-            color: 'black',
-            textTransform: 'none',
+            color: "black",
+            textTransform: "none",
             fontFamily:
               'apple-system, BlinkMacSystemFont, "Roboto", "Droid Sans", "Segoe UI", "Helvetica", Arial, sans-serif',
-            fontSize: '19px',
-            fontWeight: '400',
+            fontSize: "19px",
+            fontWeight: "400",
           }}
           onClick={() => setOpen(true)}
         >
           <FilterListIcon
             sx={{
-              marginRight: '6px',
+              marginRight: "6px",
               fontFamily:
                 "Guardian-EgypTT, Charter, 'Charter Bitstream', Cambria, 'Noto Serif Light', 'Droid Serif', Georgia, serif",
             }}
@@ -139,7 +184,13 @@ const Side = ({ setShowMarkers, setShowHeatMap, showHeatMap, data, setData, hand
       >
         <DrawerHeader>
           <IconButton onClick={handleDrawerClose}>
-            <ChevronLeftIcon sx={{ color: 'black', border: '1px solid black', borderRadius: '50%' }} />
+            <ChevronLeftIcon
+              sx={{
+                color: "black",
+                border: "1px solid black",
+                borderRadius: "50%",
+              }}
+            />
           </IconButton>
         </DrawerHeader>
         <Divider />
@@ -147,31 +198,78 @@ const Side = ({ setShowMarkers, setShowHeatMap, showHeatMap, data, setData, hand
           {photos.map((photo, index) => (
             <ListItem button key={index}>
               <FormControlLabel
-                control={<Checkbox onClick={(e) => handleSelection(e, photo.title)} checked={!!checkedItems[photo.title]} />}
+                control={
+                  <Checkbox
+                    onClick={(e) => handleSelection(e, photo.title)}
+                    checked={!!checkedItems[photo.title]}
+                  />
+                }
                 label={
-                  <Box sx={{ display: 'flex', alignItems: 'center' }}>
+                  <Box sx={{ display: "flex", alignItems: "center" }}>
                     <ListItemAvatar>
                       <Avatar
                         src={photo.src}
-                        sx={{ border: '0.5px solid #D3D3D3', height: '50px', width: '50px' }}
+                        sx={{
+                          border: "0.5px solid #D3D3D3",
+                          height: "50px",
+                          width: "50px",
+                        }}
                       />
                     </ListItemAvatar>
-                    {photo.title.includes('Italian') && photo.title.includes('2-pack') ? (
-                      <ListItemText sx={{ marginLeft: '9px' }} primary={'Italian (x2)'} />
-                    ) : photo.title.includes('Italian') && photo.title.includes('3-pack') ? (
-                      <ListItemText sx={{ marginLeft: '9px' }} primary={'Italian (x3)'} />
-                    ) : photo.src === 'https://i.etsystatic.com/12475356/r/il/72703d/4344046976/il_75x75.4344046976_az6t.jpg' ? (
-                      <ListItemText sx={{ marginLeft: '9px' }} primary={'Camino (2x)'} />
-                    ) : photo.title.includes('Bandages (3-pack)') ? (
-                      <ListItemText sx={{ marginLeft: '9px' }} primary={'Camino (x3)'} />
-                    ) : photo.title.includes('Camino') && !photo.title.includes('-') ? (
-                      <ListItemText sx={{ marginLeft: '9px' }} primary={'Camino'} secondary={<Typography variant="caption" component="span" color="black">(Colorful)</Typography>} />
-                    ) : photo.title.includes('Camino') && photo.title.includes('Plasters') ? (
-                      <ListItemText sx={{ marginLeft: '9px' }} primary={'Camino (x2)'} />
-                    ) : photo.title.includes('Stickers') ? (
-                      <ListItemText sx={{ marginLeft: '9px' }} primary={'Stickers'} />
+                    {photo.title.includes("Italian") &&
+                    photo.title.includes("2-pack") ? (
+                      <ListItemText
+                        sx={{ marginLeft: "9px" }}
+                        primary={"Italian (x2)"}
+                      />
+                    ) : photo.title.includes("Italian") &&
+                      photo.title.includes("3-pack") ? (
+                      <ListItemText
+                        sx={{ marginLeft: "9px" }}
+                        primary={"Italian (x3)"}
+                      />
+                    ) : photo.src ===
+                      "https://i.etsystatic.com/12475356/r/il/72703d/4344046976/il_75x75.4344046976_az6t.jpg" ? (
+                      <ListItemText
+                        sx={{ marginLeft: "9px" }}
+                        primary={"Camino (2x)"}
+                      />
+                    ) : photo.title.includes("Bandages (3-pack)") ? (
+                      <ListItemText
+                        sx={{ marginLeft: "9px" }}
+                        primary={"Camino (x3)"}
+                      />
+                    ) : photo.title.includes("Camino") &&
+                      !photo.title.includes("-") ? (
+                      <ListItemText
+                        sx={{ marginLeft: "9px" }}
+                        primary={"Camino"}
+                        secondary={
+                          <Typography
+                            variant="caption"
+                            component="span"
+                            color="black"
+                          >
+                            (Colorful)
+                          </Typography>
+                        }
+                      />
+                    ) : photo.title.includes("Camino") &&
+                      photo.title.includes("Plasters") ? (
+                      <ListItemText
+                        sx={{ marginLeft: "9px" }}
+                        primary={"Camino (x2)"}
+                      />
+                    ) : photo.title.includes("Stickers") ? (
+                      <ListItemText
+                        sx={{ marginLeft: "9px" }}
+                        primary={"Stickers"}
+                      />
                     ) : (
-                      <ListItemText sx={{ marginLeft: '9px' }} primary={'Italian'} />
+                      <ListItemText
+                        sx={{ marginLeft: "9px" }}
+                        primary={"Italian"}
+                      />
                     )}
                   </Box>
                 }
@@ -186,18 +284,18 @@ const Side = ({ setShowMarkers, setShowHeatMap, showHeatMap, data, setData, hand
             className="google-button"
             onClick={handleShowAll}
             sx={{
-              color: '#000000',
-              fontSize: '17px',
-              fontWeight: '400',
-              background: '#f5f5f5',
-              textTransform: 'none',
-              width: '80%',
-              border: '1px solid black',
-              cursor: 'pointer',
+              color: "#000000",
+              fontSize: "17px",
+              fontWeight: "400",
+              background: "#f5f5f5",
+              textTransform: "none",
+              width: "80%",
+              border: "1px solid black",
+              cursor: "pointer",
               fontFamily:
                 'apple-system, BlinkMacSystemFont, "Roboto", "Droid Sans", "Segoe UI", "Helvetica", Arial, sans-serif',
-              '&:hover': {
-                background: '#ccc',
+              "&:hover": {
+                background: "#ccc",
               },
             }}
           >
